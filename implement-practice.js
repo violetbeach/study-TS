534d326179d73a349ed0ba553d7070f4efaf66fc
DESKTOP-4OV6G63\user <ti6419@gmail.com>
2021-11-05 17:54:00 +0900

    Add implement practice

---
+++ implement-practice.js
@@ -0,0 +1,24 @@
+"use strict";
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
---
+++ implement-practice.ts
@@ -0,0 +1,35 @@
+interface Shape{
+    getArea(): number;
+}
+
+class Circle implements Shape {
+    radius: number;
+
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+
+class Rectangle implements Shape {
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+    }
+
+    getArea() {
+      return this.width * this.height;
+    }
+  }
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape =>{
+    console.log(shape.getArea());
+});
